a40ada69ec3514e5912329116f86dacbb8cb418d
sarawut-2542 <bigfongbear@gmail.com>
2024-11-30 12:19:53 +0700

    class0267

---
+++ app/routes/profile.tsx
@@ -0,0 +1,37 @@
+const MyProfile = () =>{
+    return(
+        <div className ="p-5 m-5 border-8 border-indigo-700 bg-zinc-400">
+            <strong>
+                My Profile
+            </strong>
+            <h1>
+            Sarawut Rungwongwat
+            </h1>
+            <h1>
+            026740491607-9
+            </h1>
+            <MyEducation />
+        </div>
+    )
+}
+
+const MyEducation = () => {
+    return (
+        <div>
+            <strong> Educations 
+
+            </strong>
+            <ul className="list-disc">
+                <li>
+                    RMUTTO 2024-Present
+                </li>
+                <li>
+                    Information Technology
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyProfile
+
