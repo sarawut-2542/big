f8b18e635c7e99c3b40ad0e89f39c072affeb3aa
sarawut-2542 <bigfongbear@gmail.com>
2024-12-07 11:56:43 +0700

    EP2

--- app/routes/blogs.tsx
+++ app/routes/blogs.tsx
@@ -1,10 +1,53 @@
 const myBlogs = () => {
-    let title = 'xxx'
+    let title = 'EP2'
     let rating = 5
-    let desciptTion = 'BBB...'
+    let descriptTion = 'เกี่ยวกัยตัวฉัน'
 
-    console.log(title)
+  
 
+    
+
+    const Comment = ({avatar, message,author,top }) => {
+        return (
+            <div>
+                <GetTop
+                top = {top}
+                />
+                <img src={avatar}
+                title={author}
+                width={50}
+                />
+                <p>{message}</p>
+                <i>{author}
+                    <hr /><br />
+                </i>
+            </div>
+        )
+    }
+
+    const  GetTop = ({top}) => {
+        if(top){
+            return '\u2764'
+        }
+        else{
+            return '';
+        }
+           
+    }
+
+    // const getRating = () => {
+    //     return <span>&#11088;&#11088;&#11088;&#11088;&#11088;</span>
+    // }
+    
+    const getRating2 = (r) => {
+        
+        let msg = ''
+        for(let i = 0; i < r ; i++){
+            msg = msg + '\u2B50'         
+        }
+        return <span>{msg}</span>;
+        //return <span>&#11088;&#11088;&#11088;&#11088;&#11088;</span>
+    }
     return (
         <div className="m-3">
             <p><strong>
@@ -13,14 +56,47 @@ const myBlogs = () => {
             </p>
             <p>
             <strong>
-                รายละเอียด : {desciptTion}
+                รายละเอียด : {descriptTion}
+            </strong>
+
+            </p>
+            <p>
+            <strong>
+                ชื่นชอบ : {getRating2(rating)}
             </strong>
 
             </p>
 
+            <p>
+            <strong>
+                อื่นๆ : ....
+            </strong>
+
+            </p>
+
+            <h1 className="text-lg font-bold">Comments</h1>
+            <br />
+            <Comment
+                avatar="public\image\snowman.png"
+                message='hello world'
+                author='sarawut'
+                top = {true}
+                />
+                <Comment
+                avatar="public\image\snowman.png"
+                message='hello world'
+                author='sarawut'
+                top = {true}
+                />
+            
+
             
         </div>
     )
+
+    
 }
+
+
 
 export default myBlogs;
